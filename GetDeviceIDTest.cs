38fb1aab84d3b69fd9f5ccb71c0c54babd53c02b
bankinginverter <bankinginverter@gmail.com>
2019-09-03 17:18:35 +0700

    added script for detecting device ID

---
+++ GetDeviceIDTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using Valve.VR;
+
+namespace Valve.VR
+{
+    public class GetDeviceIDTest : MonoBehaviour
+    {
+        void Start()
+        {
+            string id = getID(2);
+            Debug.Log("Device 2's id is " + id);
+        }
+        public string getID(uint index)
+        {
+            ETrackedPropertyError error = new ETrackedPropertyError();
+            StringBuilder sb = new StringBuilder();
+            OpenVR.System.GetStringTrackedDeviceProperty(index, ETrackedDeviceProperty.Prop_SerialNumber_String, sb, OpenVR.k_unMaxPropertyStringSize, ref error);
+            var probablyUniqueDeviceSerial = sb.ToString();
+            return probablyUniqueDeviceSerial;
+        }
+    }
+}
